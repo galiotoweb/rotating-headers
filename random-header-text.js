e85e2a3443c035f8a481c1ffa7e778876d319ee2
LeeAnne Galioto <galiotoweb@users.noreply.github.com>
2017-05-01 21:57:59 -0400

    Create random-header-text.js

---
+++ random-header-text.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    var titles = ['The Right Stuff', 'Artsy-Craftsy', 'Crackerjack Creatives', 'Pragmatic Programmer', 'The Cat's Pajamas'];
+    var i = Math.floor((Math.random()*titles.length));
+    $('#headline div h1').text(titles[i]);
+});
